cae1d0dbee2c70356eeab0baf83f85fb6194b88e
bugrahan <bugrahansahin@gmail.com>
2016-07-14 15:32:45 +0300

    namings corrected_upload_3

---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.repository;
+
+/**
+ * Created by bugrahansahin on 14/07/16.
+ */
+
+import com.example.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+    Collection<User> findByName(String name);
+    Collection<User> findByNameStartingWith(String name);
+}
